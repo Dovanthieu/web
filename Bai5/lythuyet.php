26f044dcdf0c34b0e134dbd01c4cc434a11aeadc
ThieuBka <thieubka@gmail.com>
2019-11-26 21:13:02 +0700

    first commit

---
+++ Bai5/lythuyet.php
@@ -0,0 +1,27 @@
+<?php
+
+$data='3';
+switch($data){
+    case 'ok': 
+        echo 'this is ok' ;
+    break;
+    case 'ta': 
+        echo 'this is ta' ;
+    break;
+    case 'hello': 
+        echo 'this is hello' ;
+    break;
+    default:
+    echo 'day la default'; 
+    break;
+
+}
+
+
+// 4 hàm sau gọi file php khác vào để thực thi mot hanh dong nao do
+// include():   2   ham include va include_one thuc thi ham gap loi van chay tiep .
+// include_once():
+// require():  2 ham require va require_one thuc thi co thong bao loi nhung dung lai .
+// require_once():
+
+?>
